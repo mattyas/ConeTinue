0d8fcf3b95a08cb43d4987c092d85f800a06ddf0
Mattias Allared <mattias.allared@cint.com>
2013-01-13 22:31:32 +0100

    Small refactoring

--- ConeTinue/Views/TestFailuresView.xaml.cs
+++ ConeTinue/Views/TestFailuresView.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
+using ConeTinue.Domain;
 using ConeTinue.Domain.CrossDomain;
 
 namespace ConeTinue.Views
@@ -19,107 +16,8 @@ namespace ConeTinue.Views
 
 		private void Failures_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
-			string data = "<html></html>";
 			var failure = Failures.SelectedItem as TestFailure;
-			if (failure != null)
-				data = GetHtml(failure);
-			Browser.NavigateToString(data);
-			
+			Browser.NavigateToString(FailureToHtml.GetHtml(failure));
 		}
-
-		private static string GetHtml(TestFailure failure)
-		{
-			string readAllText;
-			var stackFrame = failure.StackTrace.FirstOrDefault(x => x.HasSource);
-			if (stackFrame == null)
-			{
-				var sb = new StringBuilder();
-				sb.Append("<html>");
-				sb.Append("<h1>");
-				sb.Append(failure.TestName);
-				sb.Append("</h1>");
-				sb.Append(failure.Message);
-				sb.Append("<hr />");
-				foreach (var stack in failure.StackTrace)
-				{
-					sb.Append(stack);
-					sb.AppendLine("<br />");
-				}
-				sb.Append("</html>");
-				return sb.ToString();
-			}
-			readAllText = File.ReadAllText(stackFrame.File);
-
-
-			string codeAsHtml = new External.XtractPro.Text.CSharpSyntaxHighlighter
-				{
-					ShowCollapsibleBlocks = false, ShowComments = true, ShowHyperlinks = false, ShowLineNumbers = true, LineNumberSpaces = 4, ShowRtf = false,
-				}.Process(readAllText).Replace("<!--" + (stackFrame.Line) + "-->", "<div id=\"the_error\"><h1>" + failure.TestName + "</h1>" + failure.Message.Replace("\n", "<br />") + "</div>");
-
-			return docTemplate.Replace("[error-id]", stackFrame.Line.ToString()) + codeAsHtml + docTemplateEnd.Replace("[line-before-error]", Math.Max(1, stackFrame.Line-3).ToString());
-		}
-
-		private const string docTemplate = @"<html>
-<head>
-    <title>Source</title>
-    <style type=""text/css"">
-/* All Syntax Highlighters */
-div.sh_result { font-size: 10pt; font-family: Courier New, Verdana, Helvetica, Arial, sans-serif;background-color:#ffffff;font-size:90%; }
-span.sh_error { color: #ff0000; }
-span.sh_line { color: #008284; margin-right: 10px; border-right: 1px solid #008284; }
-span.sh_collapsed { color: #848284;border:1px solid #848284; }
-span.sh_collapsed:hover { cursor:pointer; }
-span.sh_expanded { color: #848284;text-decoration:underline; }
-span.sh_expanded:hover { cursor:pointer; }
-
-/* C#/VB.NET Syntax Highlighters */
-span.net_key { color: #0000ff; }
-span.net_type { color: #008284; }
-span.net_directive { color: #6699cc; }
-span.net_string, span.net_string a { color: #a31515; }
-span.net_comment, span.net_comment a { color: #008200; }
-span.net_xml, span.net_xml a { color: #848284; }
-
-/* XML/HTML Syntax Highlighters */
-span.xml_elem { color: #a31515; }
-span.xml_delim { color: #0000ff; }
-span.xml_att { color: #ff0000; }
-span.xml_val { color: #0000ff; }
-span.xml_text { color: #6699cc; }
-span.xml_comment { color: #008200; }
-#row_[error-id] { background-color:#ECECEC; 
-border: dashed .1em #3E62A6 !important;
-}
-#the_error {
-background-color: #FFF4F4;
-border-top: dashed .1em #3E62A6 !important;
-padding: 10px 10px 10px 74px;
-}
-
-#the_error h1 {
-font-size: 12pt; 
-font-family: Arial, sans-serif;
-font-weight: bold;
-margin: 3px;
-}</style>
-    <script type=""text/javascript"">
-function shToggle(elem, blkid)
-{
-    var blk = document.getElementById(""blk"" + blkid);
-    var expanded = (blk.style.display!=""none"");
-    blk.style.display = (expanded ? ""none"" : ""inline"");
-    elem.className = (expanded ? ""sh_collapsed"" : ""sh_expanded"");
-    return false;
-}
-
-</script>
-</head>
-<body>";
-		const string docTemplateEnd = @"    <script type=""text/javascript"">
-location.href = ""#"";
-location.href = ""#row_[line-before-error]"";
-
-</script>
-</body></html>";
 	}
 }
