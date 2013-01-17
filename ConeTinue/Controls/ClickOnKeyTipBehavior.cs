3eeb32c2a404b5e3a96ba8ce941f2e3538882631
Mattias Allared <mattias.allared@cint.com>
2013-01-17 21:47:57 +0100

    More keytips

---
+++ ConeTinue/Controls/ClickOnKeyTipBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interactivity;
+using Microsoft.Windows.Controls;
+
+namespace ConeTinue.Controls
+{
+	public class ClickOnKeyTipBehavior : Behavior<ButtonBase>
+	{
+		protected override void OnAttached()
+		{
+			KeyTipService.AddKeyTipAccessedHandler(AssociatedObject, (sender, args) => AssociatedObject.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent, AssociatedObject)));
+			base.OnAttached();
+		}
+	}
+}
---
+++ ConeTinue/Controls/SetIsSelectedOnKeyTipBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using System.Windows.Interactivity;
+using Microsoft.Windows.Controls;
+
+namespace ConeTinue.Controls
+{
+	public class SetIsSelectedOnKeyTipBehavior : Behavior<ListBoxItem>
+	{
+		protected override void OnAttached()
+		{
+			KeyTipService.AddKeyTipAccessedHandler(AssociatedObject, (sender, args) => AssociatedObject.IsSelected = true);
+			base.OnAttached();
+		}
+	}
+}
