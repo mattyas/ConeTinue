71e625a9bcd28a06caad511ef3c064f23b5585b6
Mattias Allared <mattias.allared@cint.com>
2013-11-27 13:15:57 +0100

    Added support for Visual Studio 2013

--- ConeTinue/Domain/VisualStudio/VisualStudioVersion.cs
+++ ConeTinue/Domain/VisualStudio/VisualStudioVersion.cs
@@ -12,6 +12,7 @@ namespace ConeTinue.Domain.VisualStudio
 			{
 				return new[]
 					{
+						new VisualStudioVersion {Key = "VisualStudio.DTE.12.0", Name = "Visual Studio 2013", Is80Compatible = true},
 						new VisualStudioVersion {Key = "VisualStudio.DTE.11.0", Name = "Visual Studio 2012", Is80Compatible = true},
 						new VisualStudioVersion {Key = "VisualStudio.DTE.10.0", Name = "Visual Studio 2010", Is80Compatible = true},
 						new VisualStudioVersion {Key = "VisualStudio.DTE.9.0", Name = "Visual Studio 2008", Is80Compatible = true},
